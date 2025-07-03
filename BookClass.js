a22d3dc88bdaf1502aed7cbf363252cba987b1c0
mostafa alnatshe <mostafaalnatshe4@gmail.com>
2025-07-03 03:53:55 +0300

    first commit

--- BookClass.js
+++ BookClass.js
@@ -1,1 +1,16 @@
-console.log('mostafa');
+class Book{
+constructor(title,author,isRead=false){
+ this.title = title;
+    this.author = author;
+    this.isRead = isRead;
+}
+toggleReadStatus() {
+    this.isRead = !this.isRead;
+  }
+ describe() {
+    return `📖 "${this.title}" by ${this.author} [${this.isRead ? "Read" : "Unread"}]`;
+  }
+}
+const book3 = new Book("Brave New World", "Aldous Huxley");
+book3.toggleReadStatus();
+console.log(book3.describe()); 
