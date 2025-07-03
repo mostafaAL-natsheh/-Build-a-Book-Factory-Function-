5f68a2c9e454f00dbb3c21d54065103f780c91f7
mostafa alnatshe <mostafaalnatshe4@gmail.com>
2025-07-03 03:40:10 +0300

    Factory Function

--- Book.js
+++ Book.js
@@ -1,1 +1,26 @@
-console.log('mostafa');
+
+function Book(title, author, isRead = false) {
+  const book = {};
+
+  book.title = title;
+  book.author = author;
+  book.isRead = isRead;
+
+  book.toggleReadStatus = function () {
+    this.isRead = !this.isRead;
+  };
+
+  book.describe = function () {
+    return ` "${this.title}" by ${this.author} [${this.isRead ? "Read" : "Unread"}]`;
+  };
+
+  return book;
+}
+
+const myBook = Book("The Alchemist", "Paulo Coelho");
+
+console.log(myBook.describe()); 
+
+myBook.toggleReadStatus();
+
+console.log(myBook.describe()); 
